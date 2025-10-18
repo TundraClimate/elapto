029318ba2b8ce4086b16d691695dc64adfb8940d
TundraClimate <tundra.tundere@gmail.com>
2025-10-18 22:40:50 +0900

    ✨feat: Improve display style

--- src/style.rs
+++ src/style.rs
@@ -17,23 +17,29 @@ pub enum BlockType {
 
 /// A style apply to [crate::Component].
 pub struct Style {
-    display: DisplayStyle,
+    display: OuterDisplay,
 }
 
-/// The enum in `display` properties.
-pub enum DisplayStyle {
-    /// Block style
-    Block,
-
-    /// Inline style
-    Inline,
+/// The enum in `inner-display` properties.
+pub enum InnerDisplay {
+    /// Default style
+    Flow,
 
     /// Flex style
     Flex,
 
     /// Grid style
     Grid,
+}
 
-    /// Disable style
+/// The enum in `display` properties.
+pub enum OuterDisplay {
+    /// Block style
+    Block(InnerDisplay),
+
+    /// Inline style
+    Inline(InnerDisplay),
+
+    /// Disable rendering
     None,
 }
