ed8e9e3c7e655ade8d047c3ad2268d6f8979ac71
TundraClimate <tundra.tundere@gmail.com>
2025-11-05 19:07:23 +0900

    ✨feat: Add children parser

--- macros/src/impls.rs
+++ macros/src/impls.rs
@@ -1,27 +1,178 @@
-use proc_macro2::TokenStream;
+use proc_macro2::{Span, TokenStream};
 use quote::quote;
-use syn::parse::Parse;
-use syn::{Ident, Token};
+use std::fmt::Debug;
+use syn::parse::{Parse, ParseStream};
+use syn::{Expr, Ident, Token};
 
-struct WidgetTemplate {
-    name: Ident,
+enum Node {
+    Tag(Tag),
+    Expr(Expr),
 }
 
-impl Parse for WidgetTemplate {
-    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+enum Tag {
+    WidgetTemplate {
+        name: Ident,
+        id: Option<Expr>,
+        class: Option<Expr>,
+        properties: Vec<(Ident, Expr)>,
+        childrens: Vec<Node>,
+    },
+    FragmentTemplate {
+        constant_name: Ident,
+        dummy_props: Vec<(Ident, Expr)>,
+        childrens: Vec<Node>,
+    },
+}
+
+impl Tag {
+    fn name(&self) -> &Ident {
+        match self {
+            Self::WidgetTemplate { name, .. } => name,
+            Self::FragmentTemplate { constant_name, .. } => constant_name,
+        }
+    }
+
+    fn id(&self) -> Option<&Expr> {
+        match self {
+            Self::WidgetTemplate { id, .. } => id.as_ref(),
+            Self::FragmentTemplate { .. } => None,
+        }
+    }
+
+    fn class(&self) -> Option<&Expr> {
+        match self {
+            Self::WidgetTemplate { class, .. } => class.as_ref(),
+            Self::FragmentTemplate { .. } => None,
+        }
+    }
+
+    fn properties(&self) -> &Vec<(Ident, Expr)> {
+        match self {
+            Self::WidgetTemplate { properties, .. } => properties,
+            Self::FragmentTemplate { dummy_props, .. } => dummy_props,
+        }
+    }
+
+    fn childrens(&self) -> &Vec<Node> {
+        match self {
+            Self::WidgetTemplate { childrens, .. } => childrens,
+            Self::FragmentTemplate { childrens, .. } => childrens,
+        }
+    }
+}
+
+impl Tag {
+    fn widget(
+        name: Ident,
+        id: Option<Expr>,
+        class: Option<Expr>,
+        properties: Vec<(Ident, Expr)>,
+        childrens: Vec<Node>,
+    ) -> Self {
+        Self::WidgetTemplate {
+            name,
+            id,
+            class,
+            properties,
+            childrens,
+        }
+    }
+
+    fn fragment(childrens: Vec<Node>) -> Self {
+        Self::FragmentTemplate {
+            constant_name: Ident::new("Fragment", Span::call_site()),
+            dummy_props: vec![],
+            childrens,
+        }
+    }
+}
+
+impl Parse for Tag {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
         input.parse::<Token![<]>()?;
-        let ident: Ident = input.parse()?;
+
+        if input.peek(Token![>]) {
+            input.parse::<Token![>]>()?;
+
+            let childrens = parse_childrens(input)?;
+
+            input.parse::<Token![<]>()?;
+            input.parse::<Token![/]>()?;
+            input.parse::<Token![>]>()?;
+
+            return Ok(Self::fragment(childrens));
+        }
+
+        let name: Ident = input.parse()?;
+
+        // Props
+
+        if input.peek(Token![/]) {
+            input.parse::<Token![/]>()?;
+            input.parse::<Token![>]>()?;
+
+            return Ok(Self::widget(name, None, None, vec![], vec![]));
+        }
+
         input.parse::<Token![>]>()?;
 
-        Ok(WidgetTemplate { name: ident })
+        let childrens = parse_childrens(input)?;
+
+        input.parse::<Token![<]>()?;
+        input.parse::<Token![/]>()?;
+
+        let close_name: Ident = input.parse()?;
+
+        if name != close_name {
+            return Err(syn::Error::new_spanned(close_name, "closing tag mismatch"));
+        }
+
+        input.parse::<Token![>]>()?;
+
+        Ok(Self::widget(name, None, None, vec![], childrens))
+    }
+}
+
+impl Parse for Node {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        if input.peek(Token![<]) {
+            Ok(Node::Tag(input.parse()?))
+        } else {
+            Ok(Node::Expr(input.parse()?))
+        }
+    }
+}
+
+fn parse_childrens(input: ParseStream) -> syn::Result<Vec<Node>> {
+    let mut childrens = vec![];
+
+    while !input.is_empty() {
+        if input.peek(Token![<]) && input.peek2(Token![/]) {
+            break;
+        }
+
+        let node: Node = input.parse()?;
+
+        childrens.push(node);
+    }
+
+    Ok(childrens)
+}
+
+impl Debug for Node {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Tag(tag) => write!(f, "Node::tag"),
+            Self::Expr(expr) => write!(f, "Node::expr"),
+        }
     }
 }
 
 pub(crate) fn parse_tag(tokens: TokenStream) -> syn::Result<TokenStream> {
-    let tag = syn::parse2::<WidgetTemplate>(tokens)?;
-    let name = tag.name;
+    let node = syn::parse2::<Node>(tokens)?;
+    let node = format!("{:?}", node);
 
     Ok(quote! {
-        format!("{}", stringify!(#name))
+        format!("{}", stringify!(#node))
     })
 }
