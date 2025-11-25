8d3bfbb9d8794d00fce184b83226813c13ee5f5b
TundraClimate <tundra.tundere@gmail.com>
2025-11-25 10:18:17 +0900

    ♻️refactor: Split HashCell

--- src/lib.rs
+++ src/lib.rs
@@ -6,54 +6,16 @@
 //! **THIS CRATE IS CURRENTLY BETA VERSION**  
 //! README and this docs.rs is BETA ver, Information will update always.  
 
+mod hash_cell;
 mod style;
 mod tui;
 
+use hash_cell::HashCell;
 use std::fmt::{Debug, Write};
 use std::hash::{DefaultHasher, Hash, Hasher};
 use std::sync::Arc;
 
 pub use elapto_macros::{mk, widget};
-
-#[derive(Copy, Clone, PartialEq, Eq, Hash)]
-struct HashCell(u64);
-
-impl HashCell {
-    fn new<H: Hash>(obj: H) -> Self {
-        let mut hasher = DefaultHasher::new();
-
-        obj.hash(&mut hasher);
-
-        Self(hasher.finish())
-    }
-
-    fn combine<H: Hash>(self, other: H) -> Self {
-        let mut hasher = DefaultHasher::new();
-
-        self.0.hash(&mut hasher);
-        other.hash(&mut hasher);
-
-        Self(hasher.finish())
-    }
-}
-
-impl PartialEq<u64> for HashCell {
-    fn eq(&self, other: &u64) -> bool {
-        self.0 == *other
-    }
-}
-
-impl PartialEq<HashCell> for u64 {
-    fn eq(&self, other: &HashCell) -> bool {
-        *self == other.0
-    }
-}
-
-impl Debug for HashCell {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:x}", self.0)
-    }
-}
 
 #[derive(Hash, Clone)]
 struct Identifier {
